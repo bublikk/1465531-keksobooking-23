256752644559b518d1f7f552e4ccc901f8797fa7
bublikk <yuliya.vlasovaa@gmail.com>
2021-06-13 01:09:31 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,13 +1,89 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
+import {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement} from './utils/util.js';
 
-export const getRandomInteger = (min, max) => min >= 0 && max > min ? Math.floor(min + Math.random() * (max + 1 - min)) : -1;
+const ADVERT_COUNT = 10;
+const AVATAR_MIN = 1;
+const AVATAR_MAX = 10;
+const LAT_MIN = 35.65000;
+const LAT_MAX = 35.70000;
+const LNG_MIN = 139.70000;
+const LNG_MAX = 139.80000;
+const DECIMAL_POINT = 5;
+const TITLE = 'Милая, уютная квартирка в центре Токио';
+const DESCRIPTION = 'Вашему вниманию представляется современная уютная квартира в престижном районе Токио';
+const COST_MIN = 800;
+const COST_MAX = 1200;
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+const arrayOfAvatars = [];
+const arrayOfFeatures = FEATURES.slice(getRandomPositiveInteger(0, 5));
+const arrayOfPhotos = PHOTOS.slice(getRandomPositiveInteger(0, 2));
 
-export const getRandomFloat = (min, max, point) => {
-  if (min >= 0 && max > min && point > 0) {
-    const rand = min + Math.random() * (max - min);
-    return +rand.toFixed(point);
-  }
-  return -1;
+for (let i = AVATAR_MIN; i <= AVATAR_MAX; i++) {
+  const str = `img/avatars/user${i < 10 ? `0${i}` : `${i}`}.png `;
+  arrayOfAvatars.push(str);
+}
+
+const generateLocation = () => {
+  return {
+    lat: getRandomPositiveFloat(LAT_MIN, LAT_MAX, DECIMAL_POINT),
+    lng: getRandomPositiveFloat(LNG_MIN, LNG_MAX, DECIMAL_POINT),
+  };
 };
+
+const generateOffer = () => {
+  const coordinates = generateLocation();
+
+  return {
+    author: {
+      avatar: arrayOfAvatars.shift(),
+    },
+    offer: {
+      title: TITLE,
+      address: `${coordinates.lat}, ${coordinates.lng}`,
+      price: getRandomPositiveInteger(COST_MIN, COST_MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveInteger(1, 5),
+      guests: getRandomPositiveInteger(1, 10),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: arrayOfFeatures,
+      description: DESCRIPTION,
+      photos: arrayOfPhotos,
+    },
+    location: {
+      lat: coordinates.lat,
+      lng: coordinates.lng,
+    },
+  };
+};
+
+new Array(ADVERT_COUNT).fill().map(() => generateOffer());
--- js/utils/util.js
+++ js/utils/util.js
@@ -1,7 +1,7 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-function getRandomPositiveInteger (a, b) {
+export const getRandomPositiveInteger = (min, max) => {
   // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
   // реализуем поддержку передачи минимального и максимального значения в любом порядке,
   // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
@@ -10,8 +10,8 @@ function getRandomPositiveInteger (a, b) {
   // для этого на всякий пожарный случай нижнюю границу диапазона
   // мы округляем к ближайшему большему целому с помощью Math.ceil,
   // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
   // мы не ругаем пользователя за переданное отрицательное число,
   // а просто берём его по модулю с помощью Math.abs
@@ -26,3 +26,28 @@ function getRandomPositiveInteger (a, b) {
   // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
 };
+
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+export const getRandomPositiveFloat = (min, max, digits = 1) => {
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower) + lower;
+
+  // И в конце с помощью метода toFixed любого числа в JavaScript
+  // указать требуемое количество знаков после точки
+  return +result.toFixed(digits);
+};
+
+export const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];

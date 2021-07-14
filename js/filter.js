931c1f8167b6c94d38b8a07bc5773393217cca68
bublikk <yuliya.vlasovaa@gmail.com>
2021-07-15 01:04:22 +0300

    add sort

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,18 @@
-const housingTypeSelect = document.querySelector('#housing-type');
-const housingRoomSelect = document.querySelector('#housing-rooms');
-const housingPriceSelect = document.querySelector('#housing-price');
+const houseFilter = document.querySelector('#housing-type');
+const roomFilter = document.querySelector('#housing-rooms');
+const priceFilter = document.querySelector('#housing-price');
+const guestFilter = document.querySelector('#housing-guests');
+
+const wifiFilter = document.querySelector('#filter-wifi');
+const dishwasherFilter = document.querySelector('#filter-dishwasher');
+const parkingFilter = document.querySelector('#filter-parking');
+const washerFilter = document.querySelector('#filter-washer');
+const elevatorFilter = document.querySelector('#filter-elevator');
+const conditionerFilter = document.querySelector('#filter-conditioner');
+
 
 const filterByPrice = (item) => {
-  switch (housingPriceSelect.value) {
+  switch (priceFilter.value) {
     case 'middle':
       return item.offer.price >= 10000 && item.offer.price <= 50000;
 
@@ -18,8 +27,47 @@ const filterByPrice = (item) => {
   }
 };
 
-const filter = (data) => data.filter((item) => (housingTypeSelect.value === 'any' || item.offer.type === housingTypeSelect.value)
-  && (housingRoomSelect.value === 'any' || item.offer.rooms === +housingRoomSelect.value)
-  && filterByPrice(item));
+const getFeaturesRank = (item) => {
+  let rank = 0;
+
+  if (item.offer.features !== undefined) {
+    if (item.offer.features.includes(wifiFilter.value)) {
+      rank += 1;
+    }
+    if (item.offer.features.includes(dishwasherFilter.value)) {
+      rank += 1;
+    }
+    if (item.offer.features.includes(parkingFilter.value)) {
+      rank += 1;
+    }
+    if (item.offer.features.includes(washerFilter.value)) {
+      rank += 1;
+    }
+    if (item.offer.features.includes(elevatorFilter.value)) {
+      rank += 1;
+    }
+    if (item.offer.features.includes(conditionerFilter.value)) {
+      rank += 1;
+    }
+  }
+
+  return rank;
+};
+
+const compareByFeaturesRank = (itemA, itemB) => {
+  const rankA = getFeaturesRank(itemA);
+  const rankB = getFeaturesRank(itemB);
+
+  return rankB - rankA;
+};
+
+const filter = (data) =>
+  data
+    .slice()
+    .sort(compareByFeaturesRank)
+    .filter((item) => (houseFilter.value === 'any' || item.offer.type === houseFilter.value)
+    && (roomFilter.value === 'any' || item.offer.rooms === +roomFilter.value)
+    && filterByPrice(item)
+    && (guestFilter.value === 'any' || item.offer.guests === +guestFilter.value));
 
 export {filter};

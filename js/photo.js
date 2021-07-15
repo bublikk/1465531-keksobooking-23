aba1ca5032c9db60c9001b1f709c47272395553c
bublikk <yuliya.vlasovaa@gmail.com>
2021-07-15 17:23:59 +0300

    code beautify

--- js/photo.js
+++ js/photo.js
@@ -3,12 +3,11 @@ const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const avatarChooser = document.querySelector('#avatar');
 const avatarPreview = document.querySelector('.ad-form-header__preview img');
 
-const img = document.createElement('img');
-const photoChooser = document.querySelector('#images');
-const photoPreview = document.querySelector('.ad-form__photo').appendChild(img);
 
-avatarChooser.addEventListener('change', () => {
-  const file = avatarChooser.files[0];
+const photoChooser = document.querySelector('#images');
+
+const uploadPhoto = (fileChooser, preview) => {
+  const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((extension) => fileName.endsWith(extension));
@@ -17,28 +16,23 @@ avatarChooser.addEventListener('change', () => {
     const reader = new FileReader();
 
     reader.addEventListener('load', () => {
-      avatarPreview.src = reader.result;
+      preview.src = reader.result;
     });
 
     reader.readAsDataURL(file);
   }
+};
+
+avatarChooser.addEventListener('change', () => {
+  uploadPhoto(avatarChooser, avatarPreview);
 });
 
 photoChooser.addEventListener('change', () => {
-  const file = photoChooser.files[0];
-  const fileName = file.name.toLowerCase();
+  const img = document.createElement('img');
+  const photoPreview = document.querySelector('.ad-form__photo').appendChild(img);
+  img.style.width = '100%';
+  img.style.height = '100%';
+  img.alt = 'Фотография жилья';
 
-  const matches = FILE_TYPES.some((extension) => fileName.endsWith(extension));
-
-  if (matches) {
-    const reader = new FileReader();
-
-    reader.addEventListener('load', () => {
-      photoPreview.classList.add('ad-form__photo');
-      photoPreview.src = reader.result;
-      photoPreview.alt = 'Фотография жилья';
-    });
-
-    reader.readAsDataURL(file);
-  }
+  uploadPhoto(photoChooser, photoPreview);
 });
